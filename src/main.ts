00bb6b3a2d7809e5dc63025a741269a27dc49743
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-01 18:23:31 +0300

    initialize Swagger using the SwaggerModule class

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
   app.enableCors({ credentials: true, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud storage')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(7000);
 }
