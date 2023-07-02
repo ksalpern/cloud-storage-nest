44363f2d3426d011a228c7ce2ff5a9459d552c68
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 20:18:56 +0300

    import JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[ 
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get('SECRET_KEY'),
+          signOptions: { expiresIn: configService.get('EXPIRES_IN') },
+        };
+      },
+    }),
+    UsersModule,PassportModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,LocalStrategy]
 })
 export class AuthModule {}
