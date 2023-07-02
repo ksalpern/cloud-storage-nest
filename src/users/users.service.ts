6b449808726bbc15ab62c45ec1f7220ad6e7576f
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 19:14:39 +0300

    add user methods and cleateuser class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,23 +12,19 @@ export class UsersService {
     private repository: Repository<UserEntity>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async findByEmail(email: string) {
+    return this.repository.findOneBy({
+      email,
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findById(id: number) {
+    return this.repository.findOneBy({
+      id,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  create(dto: CreateUserDto) {
+    return this.repository.save(dto);
   }
 }
