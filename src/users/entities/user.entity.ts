879afdf4a1992c22d03acfd9cdb6fdfa933d7462
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 14:01:09 +0300

    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class UserEntity {
+
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column()
+    email:string
+
+    @Column()
+    password:string
+
+    @Column()
+    fullName:string
+}
