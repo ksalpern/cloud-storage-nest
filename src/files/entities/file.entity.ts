2ff2a009066b240f436dc0509cbd5306a44987b5
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 14:10:35 +0300

    add file entity, connect user&file entities

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,27 @@
-export class File {}
+import { UserEntity } from "src/users/entities/user.entity";
+import { Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('files')
+export class FileEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    filename: string;
+  
+    @Column()
+    originalName: string;
+  
+    @Column()
+    size: number;
+  
+    @Column()
+    mimetype: string;
+  
+    @ManyToOne(() => UserEntity, (user) => user.files)
+    user: UserEntity;
+  
+    @DeleteDateColumn()
+    deletedAt?: Date;
+}
