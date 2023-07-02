8b3bfe5f34272324784f172b0c732454f601f45e
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 19:38:34 +0300

    update user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    default: 'test@test.co',
+  })
   email: string;
+
+  @ApiProperty({
+    default: 'John Martinez',
+  })
   fullName: string;
+
+  @ApiProperty({
+    default: '12345',
+  })
   password: string;
 }
