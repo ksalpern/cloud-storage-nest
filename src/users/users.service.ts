f5309ebe78ce9e520271cacd188970f1c15104ef
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 18:34:25 +0300

    add repository to service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
