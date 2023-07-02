126002a63a7300a822d9ed94025bc5e668c06cf0
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-02 14:22:40 +0300

    add db data

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { FileEntity } from './files/entities/file.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [UserEntity,FileEntity],
       synchronize: true,
     }),
     UsersModule,
